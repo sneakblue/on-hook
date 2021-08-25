45f3ce1a4b16591e1ae795124b7cd06520444fe8
david <sneakblue@gmail.com>
2021-08-25 11:04:59 -0600

    removed console logs from app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,11 @@ import HomePage from './components/HomePage';
 // import LoginFormPage from "./components/LoginFormPage";
 import * as sessionActions from './store/session';
 import Navigation from './components/Navigation';
-import { Modal } from './context/Modal';
+// import { Modal } from './context/Modal';
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
-  const [showModal, setShowModal] = useState(false);
+//   const [showModal, setShowModal] = useState(false);
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
