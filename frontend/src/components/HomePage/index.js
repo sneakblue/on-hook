56752641827580d74618d2951bc5cb26e0f9d076
david <sneakblue@gmail.com>
2021-08-25 09:50:16 -0600

    removed margin from body, footer now at bottom of page

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -5,7 +5,9 @@ import './HomePage.css';
 export default function HomePage() {
     return (
         <div className='Homepage__main--div'>
-            <h1>HomePage</h1>
+            <div className='Homepage__content--div'>
+                <h1>HomePage</h1>
+            </div>
             <Footer />
         </div>
     )
