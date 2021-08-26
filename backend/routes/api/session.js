fdf84305d88a319223d9843df2d9cfe573a214d9
david <sneakblue@gmail.com>
2021-08-26 11:47:04 -0600

    added error handling for password, confirm password

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -32,7 +32,7 @@ router.post(
       const err = new Error('Login failed');
       err.status = 401;
       err.title = 'Login failed';
-      err.errors = ['The provided credentials were invalid.'];
+      err.errors = ['Username or Password is incorrect.'];
       return next(err);
     }
 
--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -19,7 +19,7 @@ function SignupFormPage() {
     e.preventDefault();
     if (password === confirmPassword) {
       setErrors([]);
-      return dispatch(sessionActions.signup({ email, username, password }))
+      return dispatch(sessionActions.signup({ email, username, password, confirmPassword }))
         .catch(async (res) => {
           const data = await res.json();
           if (data && data.errors) setErrors(data.errors);
