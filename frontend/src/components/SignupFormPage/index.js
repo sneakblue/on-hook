becc921b343debb743966ee5ec718a75df6f66ad
david <sneakblue@gmail.com>
2021-08-26 11:10:59 -0600

    more basic styling on login modal, signup

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -71,7 +71,7 @@ function SignupFormPage() {
             required
           />
         </label>
-        <button type="submit">Sign Up</button>
+        <button className='signup__submit--btn' type="submit">Sign Up</button>
       </form>
     </div>
   );
