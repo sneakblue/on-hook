079a36a9fe4ef8cd7e2cf951ecd80f6be9e406d3
david <sneakblue@gmail.com>
2021-08-27 20:26:50 -0600

    added fishing spot page

---
+++ frontend/src/components/FishingSpotPage/FishingSpotPage.js
@@ -0,0 +1,12 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+
+export default function FishingSpotPage() {
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            <h2>Fishing Spot Page</h2>
+        </div>
+    )
+}
