dbedf0910daade4f132f36f16ab7e87ee6bd6395
david <sneakblue@gmail.com>
2021-08-30 17:40:27 -0600

    added icons for links in the footer

--- frontend/src/components/HomePage/footer.js
+++ frontend/src/components/HomePage/footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <div className='footer'>
             <div className='footer__links--div'>
-                <a href='https://www.linkedin.com/in/david-deherrera-441678208'><img className='links--logo' src='https://f0.pngfuel.com/png/887/616/linkedin-logo-linkedin-icon-linkedin-png-clip-art.png'/></a>
+                <a href='https://www.linkedin.com/in/david-deherrera-441678208'><img className='links--logo' src='http://pngimg.com/uploads/linkedIn/linkedIn_PNG4.png'/></a>
                 <a href='https://github.com/sneakblue'><img className='links--logo' src='http://pngimg.com/uploads/github/github_PNG40.png'/></a>
             </div>
         </div>
