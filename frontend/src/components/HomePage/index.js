76de027a087f6798de78636a0630559be7da6b48
david <sneakblue@gmail.com>
2021-08-30 10:50:28 -0600

    added links to homepage for fishing spots

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -2,6 +2,7 @@ import Footer from "./footer";
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getFishingSpots } from "../../store/fishing_spots";
+import { Link } from "react-router-dom";
 
 import './HomePage.css';
 
@@ -16,15 +17,22 @@ export default function HomePage() {
     return (
         <div className='Homepage__main--div'>
             <div className='Homepage__content--div'>
-                <h1>HomePage</h1>
-                {fishingSpots.map(fishingSpot => {
-                    return (
-                        <div key={fishingSpot.id}>
-                            <h4>{fishingSpot.name}</h4>
-                            <img alt={fishingSpot.id} src={fishingSpot.pic}/>
-                        </div>
-                    )
-                })}
+                <h1>Current Fishing Spots</h1>
+                <div className='Homepage__main-fishing-spot--div'>
+                    {fishingSpots.map(fishingSpot => {
+                        return (
+                            <div key={fishingSpot.id} className='home__fishing-spot--div'>
+                                <Link to={`/fishing-spot/${fishingSpot.id}`}>
+                                    <h4>{fishingSpot.name}</h4>
+
+                                </Link>
+                                <div className='home__fishing-spot-img--div'>
+                                    <img alt={fishingSpot.id} src={fishingSpot.pic} className='home__fishing-spot--img'/>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
             <Footer />
         </div>
