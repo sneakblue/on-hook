31aba6524bd5369234199be03a0c795213f86930
david <sneakblue@gmail.com>
2021-12-09 14:40:24 -0700

    added google-map-react for google maps integration

--- frontend/src/components/MapSection/index.js
+++ frontend/src/components/MapSection/index.js
@@ -1,22 +1,11 @@
-import { Loader } from "@googlemaps/js-api-loader"
+import googleMapReact from "google-map-react";
 
 function MapSection() {
     console.log(process.env.REACT_APP_MAPS_KEY)
-    const loader = new Loader({
-        apiKey: process.env.REACT_APP_MAPS_KEY,
-        version: "weekly",
-    });
-
-    loader.load().then(() => {
-        let map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: -34.397, lng: 150.644 },
-            zoom: 8,
-        });
-    });
 
 
     return (
-        <div id="map">
+        <div className="map--div">
         </div>
     )
 }
