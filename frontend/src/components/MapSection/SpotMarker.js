fa407ae67baa2061df50370ef04765ef9765ae65
david <sneakblue@gmail.com>
2021-12-09 14:51:15 -0700

    built example map

---
+++ frontend/src/components/MapSection/SpotMarker.js
@@ -0,0 +1,10 @@
+
+
+export default function SpotMarker () {
+
+    return (
+        <div>
+            <h4>Marker</h4>
+        </div>
+    )
+}
--- frontend/src/components/MapSection/index.js
+++ frontend/src/components/MapSection/index.js
@@ -1,4 +1,5 @@
 import GoogleMapReact from 'google-map-react';
+import SpotMarker from './SpotMarker';
 
 function MapSection() {
     console.log(process.env.REACT_APP_MAPS_KEY)
@@ -11,7 +12,11 @@ function MapSection() {
                 defaultCenter={{ lat: 59.95, lng: 30.33 }}
                 defaultZoom={ 11 }
             >
-                
+                <SpotMarker
+                    lat={59.955413}
+                    lng={30.337844}
+                    text="My Marker"
+                />
             </GoogleMapReact>
         </div>
     )
