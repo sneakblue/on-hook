599f617fc971ea18290999ac039e22faaf92caab
david <sneakblue@gmail.com>
2021-12-09 14:22:01 -0700

    setting up google maps component, marker

--- frontend/src/components/MapSection/index.js
+++ frontend/src/components/MapSection/index.js
@@ -1,12 +1,23 @@
-
+import { Loader } from "@googlemaps/js-api-loader"
 
 function MapSection() {
     console.log(process.env.REACT_APP_MAPS_KEY)
+    const loader = new Loader({
+        apiKey: process.env.REACT_APP_MAPS_KEY,
+        version: "weekly",
+    });
+
+    loader.load().then(() => {
+        let map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: -34.397, lng: 150.644 },
+            zoom: 8,
+        });
+    });
 
 
     return (
-        <>
-        </>
+        <div id="map">
+        </div>
     )
 }
 
