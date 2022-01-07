3386a8b0e2c2066a17afa5e035a7d8242f067646
david <sneakblue@gmail.com>
2022-01-07 16:40:35 -0700

    fixed bug for making spot from map without being logged in

--- frontend/src/components/MapSection/NewSpotMarker.js
+++ frontend/src/components/MapSection/NewSpotMarker.js
@@ -1,22 +1,39 @@
-
+import { useSelector } from "react-redux";
 
 export default function NewSpotMarker ({ setShowModal, setCreateLat, setCreateLng, lat, lng }) {
-    
+    const sessionUser = useSelector(state => state.session.user);
+
     const handleClick = () => {
         setShowModal(true);
         setCreateLat(lat);
         setCreateLng(lng);
     }
 
+    let content;
+
+    if (sessionUser) {
+        content = (
+            <>
+                <h4>Would you like to make one?</h4>
+                <button
+                    className='newSpot--btn'
+                    type='button'
+                    onClick={handleClick}
+                >Click Here!</button>
+            </>
+        )
+    } else {
+        content = (
+            <>
+                <h4>Login to Create one!</h4>
+            </>
+        )
+    }
+
     return (
         <div className='newSpot-main--div'>
             <h4>No spot here!</h4>
-            <h4>Would you like to make one?</h4>
-            <button
-                className='newSpot--btn'
-                type='button'
-                onClick={handleClick}
-            >Click Here!</button>
+            {content}
         </div>
     )
 }
