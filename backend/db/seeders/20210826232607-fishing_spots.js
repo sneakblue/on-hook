fa3c380ea0532ddd12e1ffe69e19ee9b958d0aa4
david <sneakblue@gmail.com>
2022-08-02 14:41:34 -0600

    added more spots, images to seeders. Southwest region

--- backend/db/seeders/20210826232607-fishing_spots.js
+++ backend/db/seeders/20210826232607-fishing_spots.js
@@ -4,14 +4,14 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Fishing_Spots', [
         {
-          user_id: 1,
-          description: 'This is a description of a Fishing Hole',
-          name: 'Golden Ponds',
-          city: 'Longmont',
-          state: 'CO',
-          country: 'USA',
-          lat: 40.17012,
-          lng: -105.13876
+            user_id: 1,
+            description: 'This is a description of a Fishing Hole',
+            name: 'Golden Ponds',
+            city: 'Longmont',
+            state: 'CO',
+            country: 'USA',
+            lat: 40.17012,
+            lng: -105.13876
         },
         {
             user_id: 1,
@@ -22,7 +22,137 @@ module.exports = {
             country: 'USA',
             lat: 40.18269324371436,
             lng: -105.0397492725014
-          },
+        },
+        {
+            user_id: 2,
+            description: 'Not a conventional fishing lake, but lots to be caught here.',
+            name: 'Utah Lake',
+            city: 'Orem',
+            state: 'UT',
+            country: 'USA',
+            lat: 40.23586219266547,
+            lng: -111.80505065640834
+        },
+        {
+            user_id: 3,
+            description: 'A nice lake with lots of shore fishing and plenty of room for a good boat',
+            name: 'Camanche Reservoir',
+            city: 'Wallace',
+            state: 'CA',
+            country: 'USA',
+            lat: 38.22176564455729,
+            lng: -120.97618061472154
+        },
+        {
+            user_id: 1,
+            description: 'A fishing spot to put on your list. Lots of fishing here.',
+            name: 'Lake Tahoe',
+            city: 'Homewood',
+            state: 'CA',
+            country: 'USA',
+            lat: 39.12121449452623,
+            lng: -120.11550728622802
+        },
+        {
+            user_id: 4,
+            description: 'A quaint lake. Lots of different species, and plenty of bites',
+            name: 'Folsom Lake',
+            city: 'Granite Bay',
+            state: 'CA',
+            country: 'USA',
+            lat: 38.72692595646756,
+            lng: -121.14501189045514
+        },
+        {
+            user_id: 3,
+            description: 'A fishing spot to put on your list. Lots of fishing here.',
+            name: 'San Pablo Bay',
+            city: 'Vallejo',
+            state: 'CA',
+            country: 'USA',
+            lat: 38.083984590828365,
+            lng: -122.36704281589016
+        },
+        {
+            user_id: 3,
+            description: 'A nice bay with plenty of sun and fish',
+            name: 'Monterey Bay',
+            city: 'Moss Landing',
+            state: 'CA',
+            country: 'USA',
+            lat: 36.81664010794504,
+            lng: -121.89454074047734
+        },
+        {
+            user_id: 3,
+            description: 'Not a conventional fishing lake, but lots to be caught here.',
+            name: 'Lake Mead',
+            city: 'Henderson',
+            state: 'NV',
+            country: 'USA',
+            lat: 36.159255255300856,
+            lng: -114.41918221398993
+        },
+        {
+            user_id: 3,
+            description: 'A nice lake with lots of shore fishing and plenty of room for a good boat',
+            name: 'Lake Pleasant',
+            city: 'New River',
+            state: 'AZ',
+            country: 'USA',
+            lat: 33.88323654086223,
+            lng: -112.28195102801666
+        },
+        {
+            user_id: 3,
+            description: 'Not a conventional fishing lake, but lots to be caught here.',
+            name: 'McPhee Reservoir',
+            city: 'Dolores',
+            state: 'CO',
+            country: 'USA',
+            lat: 37.51363731677932,
+            lng: -108.5472529343338
+        },
+        {
+            user_id: 5,
+            description: 'A fishing spot to put on your list. Lots of fishing here.',
+            name: 'Lake Tawakoni',
+            city: 'Point',
+            state: 'TX',
+            country: 'USA',
+            lat: 32.85289990677116,
+            lng: -95.9514930132209
+        },
+        {
+            user_id: 6,
+            description: 'A quaint lake. Lots of different species, and plenty of bites',
+            name: 'Lavon Lake',
+            city: 'Copeville',
+            state: 'TX',
+            country: 'USA',
+            lat: 33.103506310982624,
+            lng: -96.46308137847633
+        },
+        {
+            user_id: 5,
+            description: 'Not a conventional fishing lake, but lots to be caught here.',
+            name: 'Lake Walter E. Long',
+            city: 'Austin',
+            state: 'TX',
+            country: 'USA',
+            lat: 30.298944143822617,
+            lng: -97.59897152711831
+        },
+        {
+            user_id: 6,
+            description: 'Not a conventional fishing lake, but lots to be caught here.',
+            name: 'Eufaula Lake',
+            city: 'Eufaula',
+            state: 'OK',
+            country: 'USA',
+            lat: 35.30186601520205,
+            lng: -95.56310189329831
+        },
       ], {});
   },
 
--- backend/db/seeders/20220718174339-image-examples.js
+++ backend/db/seeders/20220718174339-image-examples.js
@@ -19,6 +19,58 @@ module.exports = {
         {
             url: 'https://on-hook.s3.amazonaws.com/1659464731618.jpg',
             spotId: 2
+        },
+        {
+            url: 'https://on-hook.s3.us-west-2.amazonaws.com/1659468998330.jpg',
+            spotId: 3
+        },
+        {
+            url: 'https://on-hook.s3.us-west-2.amazonaws.com/1659469274003.jpg',
+            spotId: 4
+        },
+        {
+            url: 'https://on-hook.s3.us-west-2.amazonaws.com/1659469637090.jpg',
+            spotId: 5
+        },
+        {
+            url: 'https://on-hook.s3.us-west-2.amazonaws.com/1659469874440.jpg',
+            spotId: 6
+        },
+        {
+            url: 'https://on-hook.s3.us-west-2.amazonaws.com/1659470135407.jpg',
+            spotId: 7
+        },
+        {
+            url: 'https://on-hook.s3.us-west-2.amazonaws.com/1659470433644.jpg',
+            spotId: 8
+        },
+        {
+            url: 'https://on-hook.s3.us-west-2.amazonaws.com/1659470771859.jpg',
+            spotId: 9
+        },
+        {
+            url: 'https://on-hook.s3.us-west-2.amazonaws.com/1659471059404.JPG',
+            spotId: 10
+        },
+        {
+            url: 'https://on-hook.s3.us-west-2.amazonaws.com/1659471307100.jpg',
+            spotId: 11
+        },
+        {
+            url: 'https://on-hook.s3.us-west-2.amazonaws.com/1659471568878.jpg',
+            spotId: 12
+        },
+        {
+            url: 'https://on-hook.s3.us-west-2.amazonaws.com/1659471720846.jpg',
+            spotId: 13
+        },
+        {
+            url: 'https://on-hook.s3.us-west-2.amazonaws.com/1659471946338.jpg',
+            spotId: 14
+        },
+        {
+            url: 'https://on-hook.s3.us-west-2.amazonaws.com/1659472524804.jfif',
+            spotId: 15
         }
       ], {});
   },
