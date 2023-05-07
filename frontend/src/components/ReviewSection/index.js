f4fdf75a11b71fda990ccb8c18eac84941b2aa09
sneakblue <sneakblue@gmail.com>
2023-05-07 12:05:44 -0600

    changed nearby from state variable

--- frontend/src/components/ReviewSection/index.js
+++ frontend/src/components/ReviewSection/index.js
@@ -18,7 +18,7 @@ export default function ReviewSection({ id }) {
 
     useEffect(() => {
         dispatch(getReviews(id));
-    }, [ dispatch ]);
+    }, [ dispatch, id ]);
 
     useEffect(() => {
         setRating(prevRating)
