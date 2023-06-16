ef7e067312099538653a899673dc76dfc9779b36
sneakblue <sneakblue@gmail.com>
2023-06-15 19:28:19 -0500

    made Site title link to home

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import LoginFormModal from '../LoginFormModal';
@@ -27,7 +27,9 @@ function Navigation({ isLoaded }){
 
   return (
     <div className='navbar__main--div'>
-        <h4 className='navbar--site-title-h4'>On-Hook 🎣</h4>
+        <Link exact to="/home" className='navbar--site-title--link'>
+            <h4 className='navbar--site-title-h4'>On-Hook 🎣</h4>
+        </Link>
         <div className='navbar--navlinks-div'>
             <NavLink exact to="/home" className='navbar--navlink'>Home</NavLink>
             {isLoaded && sessionLinks}
